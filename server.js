c20ea4d59cd6a57a59b517ca9efbc80378f376ba
David_Randall <oakesrandall@gmail.com>
2017-07-24 16:33:26 -0600

    Put working, trying to get delete

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -32,6 +32,8 @@ var todos = [
 app.get('/', function homepage(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/')
 
 
 /*
@@ -51,23 +53,38 @@ app.get('/api/todos/search', function search(req, res) {
 });
 
 app.get('/api/todos', function index(req, res) {
-  /* This endpoint responds with all of the todos
-   */
+  res.json({todos: todos});
 });
 
 app.post('/api/todos', function create(req, res) {
+  var newTodo = {'_id': todos.length += 1, 'task': req.body.task, 'description': req.body.description};
+  res.send(newTodo);
+
+  // var newTodo = (res.body);
+  // todos.push(newTodo);
+  // newTodo._id = todos.length;
+  // res.json({todos: todos});
+
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
 });
 
 app.get('/api/todos/:id', function show(req, res) {
+  var id = req.params.id;
+  res.send(todos[req.params.id-1]);
   /* This endpoint will return a single todo with the
    * id specified in the route parameter (:id)
    */
 });
 
 app.put('/api/todos/:id', function update(req, res) {
+  
+
+  var updateTodo = todos[req.params.id-1];
+  updateTodo.task = req.body.task;
+  updateTodo.description =  req.body.description;
+  res.send(updateTodo);
   /* This endpoint will update a single todo with the
    * id specified in the route parameter (:id) and respond
    * with the newly updated todo.
